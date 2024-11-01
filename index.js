ce36ce84ec0f1d41543f6c2d5b88261d7d4eb108
Jean <63162857+masckmaster2007@users.noreply.github.com>
2024-11-01 09:53:13 +0100

    ok, huge update

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 let gjp;
-const gdps = "https://dindegmdps.us.to/database";
-const name = "DindeGDPS";
+const gdps = "https://gdps.dimisaio.be/database";
 
 const express = require('express');
 const axios = require('axios');
-const base64 = require('base64-js');
 const fs = require('fs');
 const { inspect } = require("util");
 require("dotenv").config();
@@ -12,20 +10,20 @@ require("dotenv").config();
 const app = express();
 const PORT = 3000;
 
-  function xorCipher(string, key) {
-    return [...string]
-      .map((char, index) => String.fromCharCode(char.charCodeAt(0) ^ key.charCodeAt(index % key.length)))
-      .join('');
-  }
-  
 function encodeGJP(password) {
-  const encoded = xorCipher(password, '37526');
-  const encodedBase64 = base64.fromByteArray(Buffer.from(encoded));
-  const modifiedBase64 = encodedBase64.replace(/\+/g, '-').replace(/\//g, '_');
-  return modifiedBase64;
+  var crypto = require('crypto')
+  var shasum = crypto.createHash('sha1')
+  shasum.update(password+"mI29fmAnxgTs")
+  return shasum.digest('hex');
 }
 
 const debug = process.env.debug == "true" ? true : false;
+
+if (fs.existsSync('new')) {
+  fs.unlinkSync('new');
+  var start = (process.platform == 'darwin'? 'open': process.platform == 'win32'? 'start': 'xdg-open');
+  require('child_process').exec(start + ' ' + "http://localhost:3000");
+}
 
 if(process.env.gjp) {
   gjp = process.env.gjp;
@@ -34,29 +32,37 @@ if(process.env.gjp) {
   fs.writeFileSync("./.env", "gjp=" + gjp);
 } else {
   gjp = "null";
-  console.log("You need to (refresh) login in order to use " + name + " 1.9!\nLogin: Gear Icon => Account\nRefresh: Gear Icon => Account => More => Refresh Login");
+  console.log("You need to (refresh) login in order to use DindeGDPS 1.9!\nLogin: Gear Icon => Account\nRefresh: Gear Icon => Account => More => Refresh Login");
 }
 
 app.use(express.json({limit: '64mb'}));
 app.use(express.urlencoded({ limit: '64mb', extended: true }));
+
+app.get('*', function(req, res) {
+  res.redirect('https://gdps.dimisaio.be/19.html');
+});
 
 app.post('*', async (req, res) => {
     try {
         const path = req.path;
         const postBody = req.body;
 
-        if(debug) console.log(path + "\n\n" + inspect(req.body,false,null));
-
         // Check if accountID is present in the post body
         if (postBody.accountID) {
             // Add the 'gjp' option to the post body
-            postBody.gjp = gjp;
+            postBody.gjp2 = gjp;
         }
+        if (path.endsWith("loginGJAccount.php")) {
+            postBody.gjp2 = encodeGJP(postBody.password);
+        }
+
+        if(debug) console.log(path + "\n\n" + inspect(req.body,false,null));
 
         // Forward the modified request to another server via axios
         const response = await axios.post(`${gdps}${path}`, postBody, {
             headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'User-Agent': ''
             },
             validateStatus: () => true // Make all status codes valid
         });
@@ -72,15 +78,14 @@ app.post('*', async (req, res) => {
           fs.writeFileSync("./.env", "gjp=" + gjp);
           console.log(`Logged in as ${postBody.userName}!`);
         }
-        res.status(200);
-        res.send(`${response.data}`);
+        res.status(200).send(`${response.data}`);
     } catch (error) {
         // Handle any errors
         console.error('Error:', error);
-        res.status(500).send('Internal Server Error');
+        res.status(200).send('-1');
     }
 });
 
 app.listen(PORT, () => {
-    console.log(`QuoicouGJP is running on port ${PORT}!`);    
+    console.log(`DindeGDPS compatibility server started!`);
 });
